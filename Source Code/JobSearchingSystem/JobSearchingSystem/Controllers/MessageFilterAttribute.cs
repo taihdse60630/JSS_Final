9502c0e41f92bc68fb47ae7dcf50de715e7287ed
taihdse60630 <kogafansub@gmail.com>
2015-08-24 17:50:42 +0700

    Update MessageFilterAttribute more Final

--- Source Code/JobSearchingSystem/JobSearchingSystem/Controllers/MessageFilterAttribute.cs
+++ Source Code/JobSearchingSystem/JobSearchingSystem/Controllers/MessageFilterAttribute.cs
@@ -8,13 +8,13 @@ namespace JobSearchingSystem.Controllers
 {
     public class MessageFilterAttribute : ActionFilterAttribute
     {
-        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
         {
+            base.OnActionExecuted(filterContext);
+
             filterContext.Controller.ViewBag.successmessage = filterContext.Controller.TempData["successmessage"];
             filterContext.Controller.ViewBag.warningmessage = filterContext.Controller.TempData["warningmessage"];
             filterContext.Controller.ViewBag.errormessage = filterContext.Controller.TempData["errormessage"];
-
-            base.OnResultExecuting(filterContext);
         }
     }
 }
